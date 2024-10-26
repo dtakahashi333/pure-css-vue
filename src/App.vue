e0ea9700f17beff85ad1218a1d96f9dadaa0aef4
Daisuke Takahashi <daisuke332003@gmail.com>
2024-10-26 11:50:58 -0700

    Add button component.

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import PureButton from "./components/PureButton.vue";
+</script>
+
+<template>
+  <main>
+    <div class="content">
+      <section>
+        <h2 class="content-subhead">Default Button</h2>
+        <PureButton>A Pure Button</PureButton>
+      </section>
+      <section>
+        <h2 class="content-subhead">Disabled Button</h2>
+        <PureButton disabled>A Disabled Button</PureButton>
+      </section>
+      <section>
+        <h2 class="content-subhead">Active Button</h2>
+        <PureButton active>An Active Button</PureButton>
+      </section>
+      <section>
+        <h2 class="content-subhead">Primary Button</h2>
+        <PureButton color="primary">A Primary Button</PureButton>
+      </section>
+      <section>
+        <h2 class="content-subhead">Colored Buttons</h2>
+        <div>
+          <PureButton color="success">Buccess Button</PureButton>
+          <PureButton color="error" style="margin-left: 8px;">Buccess Button</PureButton>
+          <PureButton color="warning" style="margin-left: 8px;">Buccess Button</PureButton>
+          <PureButton color="secondary" style="margin-left: 8px;">Buccess Button</PureButton>
+        </div>
+      </section>
+      <section>
+        <h2 class="content-subhead">Colored Buttons with Rounded Corner</h2>
+        <div>
+          <PureButton color="success" rounded>Buccess Button</PureButton>
+          <PureButton color="error" rounded style="margin-left: 8px;">Buccess Button</PureButton>
+          <PureButton color="warning" rounded style="margin-left: 8px;">Buccess Button</PureButton>
+          <PureButton color="secondary" rounded style="margin-left: 8px;">Buccess Button</PureButton>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.content {
+  width: 90%;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 24px 24px;
+}
+
+.content .content-subhead {
+  color: #888;
+  font-weight: 300;
+  margin: 2em 0 1em;
+  position: relative;
+}
+
+.code {
+  border-color: #eee;
+  border-style: solid;
+  border-width: 1px 0;
+  margin-left: -1em;
+  margin-right: -1em;
+  overflow-x: auto;
+}
+</style>
---
+++ src/components/PureButton.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { reactive } from "vue";
+
+const props = defineProps({
+  disabled: {
+    type: [Boolean, String],
+    default: false,
+  },
+  active: {
+    type: [Boolean, String],
+    default: false,
+  },
+  color: {
+    type: String,
+    default: "",
+  },
+  rounded: {
+    type: [Boolean, String],
+    default: false,
+  },
+});
+// https://vuejs.org/guide/essentials/class-and-style
+const classObject = reactive({
+  "pure-button-active": props.active,
+  "pure-button-primary": props.color === "primary",
+  "button-success": props.color === "success",
+  "button-error": props.color === "error",
+  "button-warning": props.color === "warning",
+  "button-secondary": props.color === "secondary",
+  "button-rounded": props.rounded
+});
+</script>
+
+<template>
+  <button
+    class="pure-button"
+    v-bind:class="classObject"
+    v-bind:disabled="disabled"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button-success,
+.button-error,
+.button-warning,
+.button-secondary {
+  color: white;
+  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+}
+
+.button-success {
+  background: rgb(28, 184, 65);
+  /* this is a green */
+}
+
+.button-error {
+  background: rgb(202, 60, 60);
+  /* this is a maroon */
+}
+
+.button-warning {
+  background: rgb(223, 117, 20);
+  /* this is an orange */
+}
+
+.button-secondary {
+  background: rgb(66, 184, 221);
+  /* this is a light blue */
+}
+
+.button-rounded {
+  border-radius: 4px;
+}
+</style>
